f5eaf908da2579c9682177356d07b70644cf882b
MaKnok <marinaknok@gmail.com>
2022-01-19 09:05:01 -0300

    display images

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,37 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
+  const [currentImageURL, setCurrentImage] = useState('');
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string): void {
+    onOpen();
+    setCurrentImage(url);
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+        {
+          /* TODO CARD GRID */
+          cards.map(card => (
+            <Card
+              key={card.id}
+              data={card}
+              viewImage={() => handleViewImage(card.url)}
+            />
+          ))
+        }
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={currentImageURL}
+      />
     </>
   );
 }
--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,31 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent
+        mx="auto"
+        w="auto"
+        h="auto"
+        maxW={[300, 500, 900]}
+        maxH={[350, 450, 600]}
+      >
+        <ModalBody>
+          <Image
+            src={imgUrl}
+            alt="The best doge"
+            maxW={[300, 500, 900]}
+            maxH={[350, 450, 600]}
+          />
+        </ModalBody>
+
+        <ModalFooter bg="pGray.800" h="8" py="20px" borderBottomRadius={5}>
+          <Link href={imgUrl} isExternal fontSize="1rem" mr="auto">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
